11dc61df9acbe5eb41d71dfeab915954bb60ecad
Biro <luizhenriquegoncalvessilva@hotmail.com>
2026-04-24 23:57:09 -0300

    Add AuthCandidateController and JobController with authentication and job creation endpoints

---
+++ src/main/java/br/com/henriquegoncalves/gestao_vagas/modules/company/controllers/JobController.java
@@ -0,0 +1,62 @@
+package br.com.henriquegoncalves.gestao_vagas.modules.company.controllers;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.henriquegoncalves.gestao_vagas.modules.company.dto.CreateJobDTO;
+import br.com.henriquegoncalves.gestao_vagas.modules.company.entities.JobEntity;
+import br.com.henriquegoncalves.gestao_vagas.modules.company.useCases.CreateJobUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/job")
+public class JobController {
+
+        @Autowired
+        private CreateJobUseCase createJobUseCase;
+
+        @PostMapping("/")
+        @PreAuthorize("hasRole('COMPANY')")
+        @Tag(name = "Vagas", description = "Informações sobre as vagas")
+        @Operation(summary = "Cadastro de vaga", description = "Essa função é responsável por cadastrar vagas de empresas.")
+        @ApiResponses({
+                        @ApiResponse(responseCode = "200", content = {
+                                        @Content(schema = @Schema(implementation = JobEntity.class))
+                        })
+        })
+        @SecurityRequirement(name = "jwt_auth")
+        public ResponseEntity<Object> create(@Valid @RequestBody CreateJobDTO createJobDTO,
+                        HttpServletRequest request) {
+
+                Object companyId = request.getAttribute("company_id");
+                try {
+                        var jobEntity = JobEntity.builder()
+                                        .benefits(createJobDTO.getBenefits())
+                                        .description(createJobDTO.getDescription())
+                                        .level(createJobDTO.getLevel())
+                                        .companyId(UUID.fromString(companyId.toString()))
+                                        .build();
+
+                        var result = this.createJobUseCase.execute(jobEntity);
+                        return ResponseEntity.ok().body(result);
+                } catch (Exception e) {
+                        return ResponseEntity.badRequest().body(e.getMessage());
+                }
+
+        }
+}
---
+++ src/main/java/br/com/henriquegoncalves/gestao_vagas/modules/company/repositories/JobRepository.java
@@ -0,0 +1,15 @@
+package br.com.henriquegoncalves.gestao_vagas.modules.company.repositories;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.henriquegoncalves.gestao_vagas.modules.company.entities.JobEntity;
+
+
+public interface JobRepository extends JpaRepository<JobEntity, UUID> {
+
+    List<JobEntity> findByDescriptionContainingIgnoreCase(String filter);
+
+}
